a007ff906cac921c931d0e6d0678ad56d84f784d
HuWoj <hubert.wojciechowski05@htl-salzburg.ac.at>
2023-05-25 13:04:24 +0200

    Audio added

---
+++ Assets/Scripts/PlaneFlyByDelayScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlaneFlyByDelayScript : MonoBehaviour
+{
+    public AudioClip planeFlyBySound;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
